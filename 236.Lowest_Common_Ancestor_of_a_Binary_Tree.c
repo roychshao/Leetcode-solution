e3cfb23fc55fd397c07a051fb150e48c68d3d480
Roy Shao <roych.shao@gmail.com>
2021-06-30 23:24:50 +0800

    leetcode solution

---
+++ 236.Lowest_Common_Ancestor_of_a_Binary_Tree.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q)
+{
+    if(!root)
+        return NULL;
+    if(root == p || root == q)
+        return root;
+    struct TreeNode* left=lowestCommonAncestor(root->left,p,q);
+    struct TreeNode* right=lowestCommonAncestor(root->right,p,q);
+    if(left && right)
+        return root;
+    if(!left && !right)
+        return NULL;
+    if(!left)
+        return right;
+    return left;
+}
