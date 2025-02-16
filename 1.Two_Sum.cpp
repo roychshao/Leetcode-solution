7074dc8470d2b14f7d8c3f0636d005058adb4be7
roychshao <roych.shao@gmail.com>
2025-02-16 18:43:06 +0800

    re format

--- 1.Two_Sum.cpp
+++ 1.Two_Sum.cpp
@@ -5,25 +5,23 @@ using namespace std;
 class Solution {
 
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-    int i=0,find = 0;
-    vector<int>  ans;
-    for(i=0;i<nums.size();++i)
-    {
-        for(int j=i+1;j<nums.size();++j)
-        {
-            if((nums[i] + nums[j]) == target)
-            {
-                find = 1;
-                ans.push_back(i);
-                ans.push_back(j);
-                break;
-            }
+  vector<int> twoSum(vector<int> &nums, int target) {
+    int i = 0, find = 0;
+    vector<int> ans;
+    for (i = 0; i < nums.size(); ++i) {
+      for (int j = i + 1; j < nums.size(); ++j) {
+        if ((nums[i] + nums[j]) == target) {
+          find = 1;
+          ans.push_back(i);
+          ans.push_back(j);
+          break;
         }
-        if(find)    break;
+      }
+      if (find)
+        break;
     }
     return ans;
-    }
+  }
 };
 
 // test vim-fugitive
